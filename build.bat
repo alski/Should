4acdf2ebd0716668ff24cb4a1a1c940e21e010e5
AlSki <al@alski.net>
2017-02-26 17:18:59 +0000

    Fix xunit.task.runner to 2.2.0 until we know how to roll forwards

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 tools\nuget.exe install MSBuildTasks -o .build\ -version 1.5.0.214 
-tools\nuget.exe install xunit.runner.msbuild -o .build\
+tools\nuget.exe install xunit.runner.msbuild -o .build\ -version 2.2.0
 tools\nuget.exe restore src\Should.sln
 
 msbuild.exe ms.build
